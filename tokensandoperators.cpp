cdf023dd445ca759c3518a0ec379952f0ae0b9a9
Meghna-U <82393429+Meghna-U@users.noreply.github.com>
2021-05-02 11:32:35 +0530

    Update tokensandoperators.cpp

--- tokensandoperators.cpp
+++ tokensandoperators.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	  cout<<"Size of int:"<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of int:"<<sizeof(int)<<" bytes"<<endl;
     cout<<"Size of float:"<<sizeof(float)<<" bytes"<<endl;
     cout<<"Size of char:"<<sizeof(char)<<" bytes"<<endl;
     cout<<"Size of double:"<<sizeof(double)<<" bytes";
